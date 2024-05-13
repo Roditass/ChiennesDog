cecf1089af9e3e914eb9011cbc24ceae43487557
Jeanfranco02 <112710349+Jeanfranco02@users.noreply.github.com>
2024-05-12 23:47:45 -0600

    reportes

---
+++ controladores/admin/cliente.js
@@ -0,0 +1,27 @@
+// Constante para completar la ruta de la API.
+const CLIENTE_API = 'services/admin/clientes';
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'Gestionar categorías';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
+});
+
+
+/*
+*   Función para abrir un reporte parametrizado de productos de una categoría.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openReport = (id) => {
+    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
+    const PATH = new URL(`${SERVER_URL}reports/admin/productos_categoria.php`);
+    // Se agrega un parámetro a la ruta con el valor del registro seleccionado.
+    PATH.searchParams.append('idCategoria', id);
+    // Se abre el reporte en una nueva pestaña.
+    window.open(PATH.href);
+}
