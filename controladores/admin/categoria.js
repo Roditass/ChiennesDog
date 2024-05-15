a3d211c26327e37492f748187450e373e4b54dc4
DaniiRz <106101240+DaniiRz@users.noreply.github.com>
2024-05-15 12:13:48 -0600

    login

--- controladores/admin/categoria.js
+++ controladores/admin/categoria.js
@@ -15,6 +15,8 @@ const SAVE_FORM = document.getElementById('saveForm'),
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
+        // Llamada a la función para mostrar el encabezado y pie del documento.
+        loadTemplate();
     // Llamada a la función para llenar la tabla con los registros existentes.
     fillTable();
 });
