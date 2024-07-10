0e64236b7d346e76d7a8e88ad0074e6d2150d301
Jhonnyy0 <jhonnyamaya168@gmail.com>
2024-07-10 12:06:56 -0600

    comentarios

--- mobile/ChiennesDog/components/buttons/defaultBtn.js
+++ mobile/ChiennesDog/components/buttons/defaultBtn.js
@@ -1,27 +1,31 @@
 import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+// Definición del componente funcional defaultBtn
 export default function defaultBtn({textoBoton, accionBoton}) {
 
     return(
-        <>
+        // TouchableOpacity es un componente que responde a la interacción del usuario
         <TouchableOpacity style={styles.button} onPress={accionBoton}>
             <Text style={styles.buttonText}>{textoBoton}</Text>
         </TouchableOpacity>
-        </>
     );
 }
 
+// Creación de los estilos para los componentes
 const styles = StyleSheet.create({
 
+    // Estilo para el botón
     button: {
-        width: "80%",
-        borderRadius: 20,
-        backgroundColor: "#E65E0A",
-        padding: 10,
+        width: "80%", // Ancho del botón del 80% del contenedor padre
+        borderRadius: 20, // Bordes redondeados con un radio de 20
+        backgroundColor: "#E65E0A", // Color de fondo del botón (naranja)
+        padding: 10, // Espaciado interno del botón
     },
+    // Estilo para el texto dentro del botón
     buttonText: {
-        textAlign: 'center',
-        color: "#FFF",
-        fontWeight: 'medium',
-        fontSize: 25,
+        textAlign: 'center', // Alineación del texto al centro
+        color: "#FFF", // Color del texto (blanco)
+        fontWeight: 'medium', // Grosor de la fuente (medio)
+        fontSize: 25, // Tamaño de la fuente
     }
 });
--- mobile/ChiennesDog/components/inputs/correoInput.js
+++ mobile/ChiennesDog/components/inputs/correoInput.js
@@ -1,35 +1,42 @@
 import { View, TextInput, StyleSheet, Text, Dimensions } from "react-native";
 import React, { useState } from 'react';
 
+// Definición del componente funcional CorreoInput
 export default function CorreoInput({
-    placeHolder,
-    setValor,
-    contra,
-    setTextChange,
-    texto,
-    bloqueado,  
+    placeHolder,   // Propiedad para el placeholder del TextInput
+    setValor,      // Valor del TextInput
+    contra,        // Booleano para mostrar el texto como contraseña
+    setTextChange, // Función para manejar el cambio de texto
+    texto,         // Texto opcional a mostrar arriba del TextInput
+    bloqueado,     // Booleano para determinar si el TextInput está editable
 }) {
+    // Estado para manejar el valor del correo electrónico
     const [email, setEmail] = useState('');
+    // Estado para manejar el mensaje de error
     const [errorMessage, setErrorMessage] = useState("");
-
+    // Función para validar el formato del correo electrónico usando una expresión regular
     const validarCorreo = (correo) => {
         // Expresión regular para validar correo electrónico
         const re = /\S+@\S+\.\S+/;
+        // Retorna true si el correo es válido, de lo contrario false
         return re.test(correo);
     };
-
+    // Función para manejar el cambio de texto en el TextInput
     const handleTextChange = (texto) => {
+        // Verifica si el texto contiene letras mayúsculas
         const hasUpperCase = /[A-Z]/.test(texto);
         if (hasUpperCase) {
-            setErrorMessage('No se permiten mayúsculas');
+            setErrorMessage('No se permiten mayúsculas'); // Establece un mensaje de error si hay mayúsculas
         } else {
+        // Actualiza el estado del correo electrónico
         setEmail(texto);
         if (!validarCorreo(texto)) {
-            setErrorMessage('Correo electrónico no válido');
+            setErrorMessage('Correo electrónico no válido'); // Establece un mensaje de error si el correo no es válido
         } else {
-            setErrorMessage('');
+            setErrorMessage(''); // Limpia el mensaje de error si el correo es válido
         }
-        setTextChange(texto); }
+        setTextChange(texto); // Llama a la función setTextChange con el texto actualizado
+        }
     };
     return (
         <View style={styles.inputConteiner}>
@@ -51,19 +58,20 @@ export default function CorreoInput({
     );
 }
 
+// Creación de los estilos para los componentes
 const styles = StyleSheet.create({
     inputConteiner: {
-        backgroundColor: "#FFE6D5",
-        padding: 12,
-        paddingLeft: 22,
-        borderRadius: 20,
-        marginTop: "1%",
-        width: "90%",
-        alignSelf: "center",
-        marginVertical: "7%",
+        backgroundColor: "#FFE6D5", // Color de fondo del contenedor del input
+        padding: 12, // Espaciado interno
+        paddingLeft: 22, // Espaciado interno a la izquierda
+        borderRadius: 20, // Bordes redondeados
+        marginTop: "1%", // Margen superior
+        width: "90%", // Ancho del contenedor (90% del contenedor padre)
+        alignSelf: "center", // Centrar el contenedor horizontalmente
+        marginVertical: "7%", // Margen vertical
     },
     input: {
-        fontSize: 22,
-        fontWeight: "medium",
+        fontSize: 22, // Tamaño de la fuente
+        fontWeight: "medium", // Grosor de la fuente (medio)
     },
 });
